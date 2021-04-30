08e99d7204e6721d9e14aab0358c7fd09229f109
Jamie Hill <jalhill01@gmail.com>
2021-04-29 20:09:36 -0400

    Added the Index View and NotListItem model

---
+++ Barbervill.Models/NoteListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbervill.Models
+{
+    public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [DisplayName ="Time of Creation"]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+    }
+}
---
+++ Barberville.Data/Migrations/202104290223552_initial migration of Barberville.cs
@@ -0,0 +1,16 @@
+namespace Barberville.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialmigrationofBarberville : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
